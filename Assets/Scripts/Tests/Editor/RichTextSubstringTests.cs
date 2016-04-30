94b2be281e5acb850a1208f470e617ff3c361c1b
Atul Varma <varmaa@gmail.com>
2016-04-30 09:19:34 -0400

    Add naive RichTextSubstring w/ some (failing) tests.

---
+++ Assets/Scripts/Tests/Editor/RichTextSubstringTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+public class RichTextSubstringTests {
+	[Test]
+	public void ItSetsPlainText () {
+		RichTextSubstring rts = new RichTextSubstring("<i>foo</i> bar");
+		Assert.AreEqual ("foo bar", rts.plainText);
+	}
+
+	[Test]
+	public void SubstringReturnsWellFormattedRichText() {
+		RichTextSubstring rts = new RichTextSubstring("<i>foo</i> bar");
+
+		Assert.AreEqual ("<i>f</i>", rts.Substring (1));
+		Assert.AreEqual ("<i>fo</i>", rts.Substring (2));
+		Assert.AreEqual ("<i>foo</i>", rts.Substring (3));
+		Assert.AreEqual ("<i>foo</i> ", rts.Substring (4));
+		Assert.AreEqual ("<i>foo</i> b", rts.Substring (5));
+		Assert.AreEqual ("<i>foo</i> ba", rts.Substring (6));
+		Assert.AreEqual ("<i>foo</i> bar", rts.Substring (7));
+	}
+}
---
+++ Assets/Scripts/Utils/RichTextSubstring.cs
@@ -0,0 +1,31 @@
+﻿public class RichTextSubstring
+{
+	public readonly string richText;
+	public readonly string plainText;
+
+	public RichTextSubstring (string richText) {
+		this.richText = richText;
+		this.plainText = string.Empty;
+
+		bool isInTag = false;
+
+		for (int i = 0; i < richText.Length; i++) {
+			char c = richText [i];
+			if (isInTag) {
+				if (c == '>') {
+					isInTag = false;
+				}
+			} else {
+				if (c == '<') {
+					isInTag = true;
+				} else {
+					this.plainText += c;
+				}
+			}
+		}
+	}
+
+	public string Substring(int length) {
+		return string.Empty;
+	}
+}
